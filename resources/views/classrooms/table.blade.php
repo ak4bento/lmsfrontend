66c983f0f27438c106e88223189d8832f72665e4
tenriajeng <ilhamtenriajeng03@gmail.com>
2021-04-05 17:16:22 +0800

    fixing admin menu

--- resources/views/classrooms/table.blade.php
+++ resources/views/classrooms/table.blade.php
@@ -2,30 +2,35 @@
     <table id="example2" class="table table-bordered table-striped">
         <thead>
             <tr>
+                <th>No</th>
                 <th>Mata Kuliah</th>
                 <th>Periode Mengajar</th>
                 <th>Slug</th>
                 <th>Kode Kelas</th>
                 <th>Judul</th>
                 <th>Deskripsi</th>
-                <th>Jam</th> 
+                <th>Jam</th>
                 <th>Dibuat Oleh</th>
                 <th>Aksi</th>
             </tr>
         </thead>
         <tbody>
-        @foreach($classrooms as $classroom)
-            <tr>
-                <td>{{ App\Models\Subject::find($classroom->subject_id)->title  }}</td>
-                <td>{{ App\Models\TeachingPeriod::find($classroom->teaching_period_id)->name }}</td>
-                <td>{{ $classroom->slug }}</td>
-                <td>{{ $classroom->code }}</td>
-                <td>{{ $classroom->title }}</td>
-                <td>{{ $classroom->description }}</td>
-                <td>{{ $classroom->start_at }} - {{ $classroom->end_at }}</td>
-                <td>{{ App\Models\User::find($classroom->created_by)->name  }}</td>
-                <td width="120">
-                    {!! Form::open(['route' => ['classrooms.destroy', $classroom->id], 'method' => 'delete']) !!}
+            @php
+                $no = 1;
+            @endphp
+            @foreach ($classrooms as $classroom)
+                <tr>
+                    <td>{{ $no++ }}</td>
+                    <td>{{ App\Models\Subject::find($classroom->subject_id)->title }}</td>
+                    <td>{{ App\Models\TeachingPeriod::find($classroom->teaching_period_id)->name }}</td>
+                    <td>{{ $classroom->slug }}</td>
+                    <td>{{ $classroom->code }}</td>
+                    <td>{{ $classroom->title }}</td>
+                    <td>{{ $classroom->description }}</td>
+                    <td>{{ $classroom->start_at }} - {{ $classroom->end_at }}</td>
+                    <td>{{ App\Models\User::find($classroom->created_by)->name }}</td>
+                    <td width="120">
+                        {!! Form::open(['route' => ['classrooms.destroy', $classroom->id], 'method' => 'delete']) !!}
                         <a href="{{ route('classrooms.show', [$classroom->id]) }}" class='btn btn-default btn-sm'>
                             <i class="far fa-eye"></i>
                         </a>
@@ -33,10 +38,10 @@
                             <i class="far fa-edit"></i>
                         </a>
                         {!! Form::button('<i class="far fa-trash-alt"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-sm', 'onclick' => "return confirm('Are you sure?')"]) !!}
-                    {!! Form::close() !!}
-                </td>
-            </tr>
-        @endforeach
+                        {!! Form::close() !!}
+                    </td>
+                </tr>
+            @endforeach
         </tbody>
     </table>
 </div>
