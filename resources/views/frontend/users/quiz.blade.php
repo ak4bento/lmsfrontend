bde89e678e8d3f2bbc5c529a525a0312fa3d898a
tenriajeng <ilhamtenriajeng03@gmail.com>
2021-04-15 16:28:27 +0800

    quiz

--- resources/views/frontend/users/quiz.blade.php
+++ resources/views/frontend/users/quiz.blade.php
@@ -1,96 +1,95 @@
 @extends('frontend.layouts.app') @section('content')
-    <div class="container">
-        <div class="jumbotron jumbotron-fluid text-white" style="background-color: #174ea6">
-            <div class="container">
-                <h1 class="display-4">Discover</h1>
-                <p class="lead">Temukan kelas terbaik untuk anda.</p>
-            </div>
+<div class="container">
+    <div class="jumbotron jumbotron-fluid text-white" style="background-color: #174ea6">
+        <div class="container">
+            <h1 class="display-4">Discover</h1>
+            <p class="lead">Temukan kelas terbaik untuk anda.</p>
         </div>
-        <div class="row">
-            <div class="col-md-12 col-lg-12 col-sm-12">
-                <div class="row">
-                    <div class="col-lg-3 col-md-12 col-sm-12">
-                        <div class="row" style="margin-bottom:10px;">
-                            <div class="col-12">
-                                <!-- Profile Image -->
-                                <div class="card card-primary card-outline">
-                                    <div class="card-body box-profile">
-                                        <h1 class="text-center">56:30</h1>
-                                    </div>
-                                    <!-- /.card-body -->
+    </div>
+    <div class="row">
+        <div class="col-md-12 col-lg-12 col-sm-12">
+            <div class="row">
+                <div class="col-lg-3 col-md-12 col-sm-12">
+                    <div class="row" style="margin-bottom:10px;">
+                        <div class="col-12">
+                            <!-- Profile Image -->
+                            <div class="card card-primary card-outline">
+                                <div class="card-body box-profile">
+                                    <h1 class="text-center" id="time">
+                                        {{ Session::get('timerStartQuiz')}}
+                                        <br>
+                                        {{ Session::get('timerEndQuiz')}}
+                                    </h1>
                                 </div>
-                                <!-- /.card -->
-                                <a href="quiz_submit.html">
-                                    <button type="button" class="btn btn-block btn-warning btn-lg">Submit</button>
-                                </a>
+                                <!-- /.card-body -->
                             </div>
+                            <!-- /.card -->
+                            <a href="quiz_submit.html">
+                                <button type="button" class="btn btn-block btn-warning btn-lg">Submit</button>
+                            </a>
                         </div>
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="card card-primary card-outline">
-                                    <div class="card-body">
-                                        <div class="align-items-center" style="text-align:center">
-                                            @foreach ($question as $item)
-                                                <button style="margin-bottom:2px;width:23%" type="button"
-                                                    class="btn btn-default">
-                                                    {{ $item->id }}</button>
-                                            @endforeach
-                                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="card card-primary card-outline">
+                                <div class="card-body">
+                                    <div class="align-items-center" style="text-align:center">
+                                        @foreach ($question as $key => $data)
+                                        <button style="margin-bottom:2px;width:23%" type="button"
+                                            class="btn btn-default">
+                                            {{ ++$key }}
+                                        </button>
+                                        @endforeach
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-9 col-md-12 col-sm-12">
-                        <div class="row">
-                            <div class="col-md-12">
-                                <!-- Box Comment -->
-                                <div class="card card-widget">
-                                    <div class="card-header">
-                                        <div class="user-block">
-                                            <img class="img-circle" src="https://img.icons8.com/carbon-copy/2x/file.png"
-                                                alt="User Image">
-                                            <span class="username"><a href="#">Soal 1</a></span>
-                                            <span class="description">of {{ $question->count() }}</span>
-                                        </div>
-                                        <!-- /.user-block -->
-                                        <div class="card-tools">
-                                            <button type="button" class="btn btn-primary">
-                                                <i class="fas fa-angle-left"></i>
-                                            </button>
-                                            <button type="button" class="btn btn-primary">
-                                                <i class="fas fa-angle-right"></i>
-                                            </button>
-                                        </div>
-                                        <!-- /.card-tools -->
+                </div>
+                <div class="col-lg-9 col-md-12 col-sm-12">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <!-- Box Comment -->
+                            <div class="card card-widget">
+                                <div class="card-header">
+                                    <div class="user-block">
+                                        <img class="img-circle" src="https://img.icons8.com/carbon-copy/2x/file.png"
+                                            alt="User Image">
+                                        <span class="username"><a href="#">Soal 1</a></span>
+                                        <span class="description">of {{ $question->count() }}</span>
                                     </div>
-                                    <!-- /.card-header -->
+                                    <!-- /.user-block -->
+                                    <div class="card-tools">
+                                        <button type="button" class="btn btn-primary">
+                                            <i class="fas fa-angle-left"></i>
+                                        </button>
+                                        <button type="button" class="btn btn-primary">
+                                            <i class="fas fa-angle-right"></i>
+                                        </button>
+                                    </div>
+                                    <!-- /.card-tools -->
                                 </div>
-                                <!-- /.card -->
+                                <!-- /.card-header -->
                             </div>
-                            <!-- /.col -->
-                            <div class="col-md-12">
-                                <!-- Box Comment -->
-                                <div class="card card-widget">
-                                    <div class="card-body">
-                                        <!-- post text -->
-                                        <div id="question">
+                            <!-- /.card -->
+                        </div>
+                        <!-- /.col -->
+                        <div class="col-md-12">
+                            <!-- Box Comment -->
+                            <div class="card card-widget">
+                                <div class="card-body">
+                                    <!-- post text -->
+                                    <div id="question">
 
-                                        </div>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-md-12">
-                                <div class="card card-widget">
-                                    <div class="card-body">
-                                        <div class="form-group" style="font-size: large;">
-                                            <div class="custom-control custom-radio">
-                                                <input class="custom-control-input" type="radio" id="customRadio1"
-                                                    name="customRadio">
-                                                <label for="customRadio1" class="custom-control-label">Custom Radio</label>
-                                            </div>
-                                            <hr>
-                                        </div>
+                        </div>
+                        <div class="col-md-12">
+                            <div class="card card-widget">
+                                <div class="card-body">
+                                    <div class="form-group" style="font-size: large;" id="itemOption">
+
                                     </div>
                                 </div>
                             </div>
@@ -100,32 +99,54 @@
             </div>
         </div>
     </div>
+</div>
 @endsection
 @push('page_scripts')
-    <script>
-        $(document).ready(function() {
-            var id = {{ isset($question->id) ? $question->id : '1' }}
-            console.log("ini ID :", id);
-            var rute = "{{ url('get-question') }}/" + id;
-            console.log("ini rute :", rute);
-            $.ajax({
-                url: rute,
-                type: 'get',
-                success: function(response) {
-                    console.log("ini re :", response.question);
-                    // var i = 0;
-                    $.each(response, function(key, value) {
-
-                        console.log("ini mi responya :", response.question.content);
-                        var question = " <p> " +
-                            response.question.content + "</p>";
-                        $("#question").append(question);
-
-                        i++;
-                    });
-                }
-            });
+<script>
+    $(document).ready(function() {
+        var id = {{ $quiz->id }};
+        console.log("ini ID :", id);
+        var rute = "{{ url('get-question') }}/" + id;
+        console.log("ini rute :", rute);
+        $.ajax({
+            url: rute,
+            type: 'get',
+            success: function(response) {
+                console.log("ini re :", response.length);
+                var question = " <p> " + response[0].content + "</p>";
+                $("#question").append(question);
+                $.each(response, function(key, value) {
+                    console.log("ini mi responya :", response.choceItems);
+                    var option = '<div class="custom-control custom-radio"> <input class="custom-control-input" type="radio" id="customRadio1" name="customRadio"> <label for="customRadio1" class="custom-control-label">'+
+                                response[key].choice_text
+                                    +'</label></div><hr>';
+                    $("#itemOption").append(option);
+                }); 
+            }
         });
+    });
+    function startTimer(duration, display) {
+        var timer = duration, minutes, seconds;
+        setInterval(function () {
+            minutes = parseInt(timer / 60, 10);
+            seconds = parseInt(timer % 60, 10);
 
-    </script>
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+
+            display.textContent = minutes + ":" + seconds;
+
+            if (--timer < 0) {
+                timer = duration;
+            }
+            
+        }, 1000);
+    }
+
+    window.onload = function () {
+        var fiveMinutes = 60 * 60,
+            display = document.querySelector('#time');
+        startTimer(fiveMinutes, display);
+    };
+</script>
 @endpush
