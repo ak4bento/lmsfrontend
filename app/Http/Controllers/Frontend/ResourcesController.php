289527fbce7a5d205d6e63e33cb56a372d692fec
akilsagitarius <hacker.akil17@gmail.com>
2021-04-29 14:47:43 +0800

    url link

--- app/Http/Controllers/Frontend/ResourcesController.php
+++ app/Http/Controllers/Frontend/ResourcesController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Frontend;
- 
+
 use App\Repositories\ResourceRepository;
 use App\Http\Controllers\AppBaseController;
 use Illuminate\Http\Request;
@@ -18,7 +18,7 @@ use App\Models\TeachableUser;
 class ResourcesController extends AppBaseController
 {
     /** @var  ResourceRepository */
-    private $resourceRepository; 
+    private $resourceRepository;
     private $teachableRepository;
 
     public function __construct(TeachableRepository $teachableRepo,ResourceRepository $resourceRepo)
@@ -33,7 +33,7 @@ class ResourcesController extends AppBaseController
      * @return \Illuminate\Http\Response
      */
     public function create($slug)
-    { 
+    {
         // dd($slug);
         $classrooms = DB::table('classrooms')
                     ->join('subjects', 'subjects.id', '=', 'classrooms.subject_id')
@@ -52,7 +52,7 @@ class ResourcesController extends AppBaseController
     }
 
     public function store(Request $request)
-    { 
+    {
         $validated = $request->validate([
             'title' => 'required|unique:resources,title',
             'file' => 'required|mimes:doc,docx,pdf,mp4,mp3'
@@ -66,15 +66,15 @@ class ResourcesController extends AppBaseController
         $collection_name = $request->file('file')->extension();
         switch($collection_name)
         {
-            case 'mp4'  : 
+            case 'mp4'  :
                 $collection_name = 'video';
                 $input['type']   = 'video';
                 break;
-            case 'mp3'  : 
+            case 'mp3'  :
                 $collection_name = 'audio';
                 $input['type']   = 'audio';
                 break;
-            default     : 
+            default     :
                 $collection_name = 'files';
                 $input['type']   = 'documents';
                 break;
@@ -89,24 +89,24 @@ class ResourcesController extends AppBaseController
         $data['order_column'] = '1';
         $data['media_type'] = 'resource';
         $data['size'] = $files->getSize();
-        
+
         $input['created_by'] = auth()->user()->id;
         $input['final_grade_weight'] = 0;
         $input['order'] = 1;
         $input['max_attempts_count']=1;
         $input['pass_threshold']=1;
-        $input['data']='video';
+        $input['data']= 'http://lms.sipsedutech.id/files/'.$fileName;
 
-        
+
         $resource = $this->resourceRepository->create($input);
         $input['teachable_type'] = "resource";
         $input['teachable_id'] = $resource['id'];
-        
-        $teachable = $this->teachableRepository->create($input); 
-        
+
+        $teachable = $this->teachableRepository->create($input);
+
         $data['media_id'] = $resource['id'];
         $data['custom_properties'] = json_encode(array('user' => Auth::user()->id));
-        
+
         Media::create($data);
         if(isset($input['user_id'])){
             foreach($input['user_id'] as $user_id){
@@ -117,12 +117,12 @@ class ResourcesController extends AppBaseController
                     $value['teachable_id'] = $teachable['id'];
                     $deleted_at['deleted_at'] = date('d/m/Y H:i:s');
                     $TeachableUser = TeachableUser::where('teachable_id',$value['teachable_id'])->delete();
-                    
+
                     TeachableUser::create($value);
                 }
-            } 
+            }
         }
-        $files->move('images',$files->getClientOriginalName());
+        $files->move('files',$files->getClientOriginalName());
 
         Alert::success('Materi saved successfully.');
         return redirect()->route('classroom.detail', $input['slug']);
@@ -162,33 +162,33 @@ class ResourcesController extends AppBaseController
     }
 
     public function update($id, Request $request)
-    {  
+    {
         $teachable = DB::table('teachables')->where('teachable_id',$id)->where('teachable_type','resource')->where('deleted_at',null)->select('*')->first();
         $model = Media::where('media_type', 'resource')->where('media_id',$id)->first();
         $files = $request->file('file');
         $input = $request->all();
         $input['type']='video';
 
-        if ($files != null) { 
+        if ($files != null) {
             $collection_name = $request->file('file')->extension();
             switch($collection_name)
             {
-                case 'mp4'  : 
+                case 'mp4'  :
                     $collection_name = 'video';
                     $input['type']   = 'video';
                     break;
-                case 'mp3'  : 
+                case 'mp3'  :
                     $collection_name = 'audio';
                     $input['type']   = 'audio';
                     break;
-                default     : 
+                default     :
                     $collection_name = 'files';
                     $input['type']   = 'documents';
                     break;
             }
             $fileName = $files->getClientOriginalName();
             $name = pathinfo($fileName, PATHINFO_FILENAME);
-    
+
             $data['name'] = $name;
             $data['file_name'] = $fileName;
             $data['collection_name'] = $collection_name;
@@ -211,9 +211,9 @@ class ResourcesController extends AppBaseController
         $input['teachable_type'] = "resource";
         $input['teachable_id'] = $resource['id'];
         $input['classroom_id'] = $input['classroom_id'];
-        
-        $teachable = $this->teachableRepository->update($input, $teachable->id); 
-        
+
+        $teachable = $this->teachableRepository->update($input, $teachable->id);
+
         $data['media_id'] = $resource['id'];
         $data['custom_properties'] = json_encode(array('user' => Auth::user()->id));
 
@@ -228,7 +228,7 @@ class ResourcesController extends AppBaseController
                     $TeachableUser = TeachableUser::where('teachable_id',$value['teachable_id'])->delete();
                     TeachableUser::create($value);
                 }
-            } 
+            }
         }else{
             $TeachableUser = TeachableUser::where('teachable_id',$teachable['id'])->delete();
         }
