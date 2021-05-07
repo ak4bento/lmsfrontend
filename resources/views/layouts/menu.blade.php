6799b6303f06ff77eb2a7ac97167e08a5a40c43a
tenriajeng <ilhamtenriajeng03@gmail.com>
2021-05-07 15:52:34 +0800

    modify backpack

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -15,19 +15,20 @@
 </li>
 
 <li class="nav-item">
-    <a href="{{ route('modelHasRoles.index') }}"
-       class="nav-link {{ Request::is('modelHasRoles*') ? 'active' : '' }}">
-       <i class="nav-icon fas fa-users"></i>
+    <a href="{{ route('modelHasRoles.index') }}" class="nav-link {{ Request::is('modelHasRoles*') ? 'active' : '' }}">
+        <i class="nav-icon fas fa-users"></i>
         <p>Manajement Pengguna</p>
     </a>
 </li>
 
-{{-- <li class="nav-item">
+{{--
+<li class="nav-item">
     <a href="{{ route('userTeachers.index') }}" class="nav-link {{ Request::is('userTeachers*') ? 'active' : '' }}">
         <i class="nav-icon fas fa-graduation-cap"></i>
         <p style="font-size: 18px;">Daftar Dosen</p>
     </a>
-</li> --}}
+</li>
+--}}
 
 <li class="nav-item">
     <a href="{{ route('subjects.index') }}" class="nav-link {{ Request::is('subjects*') ? 'active' : '' }}">
@@ -36,15 +37,12 @@
     </a>
 </li>
 
-
 <li class="nav-item">
-    <a href="{{ route('teachingPeriods.index') }}"
-        class="nav-link {{ Request::is('teachingPeriods*') ? 'active' : '' }}">
+    <a href="{{ route('teachingPeriods.index') }}" class="nav-link {{ Request::is('teachingPeriods*') ? 'active' : '' }}">
         <i class="nav-icon fas fa-calendar"></i>
         <p style="font-size: 18px;">Tahun Ajaran</p>
     </a>
 </li>
-
 
 <li class="nav-item">
     <a href="{{ route('classrooms.index') }}" class="nav-link {{ Request::is('classrooms*') ? 'active' : '' }}">
@@ -53,23 +51,25 @@
     </a>
 </li>
 
-{{-- <li class="nav-item">
-    <a href="{{ route('classroomUsers.index') }}"
-class="nav-link {{ Request::is('classroomUsers*') ? 'active' : '' }}">
-<p style="font-size: 18px;">Classroom Users</p>
-</a>
+{{--
+<li class="nav-item">
+    <a href="{{ route('classroomUsers.index') }}" class="nav-link {{ Request::is('classroomUsers*') ? 'active' : '' }}">
+        <p style="font-size: 18px;">Classroom Users</p>
+    </a>
 </li>
-
 
 <li class="nav-item">
     <a href="{{ route('profiles.index') }}" class="nav-link {{ Request::is('profiles*') ? 'active' : '' }}">
         <p style="font-size: 18px;">Profiles</p>
     </a>
-</li> --}}
-<li class="nav-item 
+</li>
+--}}
+<li
+    class="nav-item 
     {{ Request::is('questions*') ? 'menu-is-opening menu-open' : '' }}
     {{ Request::is('quizzes*') ? 'menu-is-opening menu-open' : '' }}
-    ">
+    "
+>
     <a href="#" class="nav-link">
         <i class="nav-icon fa fa-question"></i>
         <p>
@@ -78,7 +78,6 @@ class="nav-link {{ Request::is('classroomUsers*') ? 'active' : '' }}">
         </p>
     </a>
     <ul class="nav nav-treeview">
-
         <li class="nav-item">
             <a href="{{ route('quizzes.index') }}" class="nav-link {{ Request::is('quizzes*') ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
@@ -86,8 +85,7 @@ class="nav-link {{ Request::is('classroomUsers*') ? 'active' : '' }}">
             </a>
         </li>
         <li class="nav-item">
-            <a href="{{ route('questions.index') }}"
-                class="nav-link {{ Request::is('questions*') ? 'active' : '' }}">
+            <a href="{{ route('questions.index') }}" class="nav-link {{ Request::is('questions*') ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
 
                 <p>Soal</p>
@@ -96,78 +94,63 @@ class="nav-link {{ Request::is('classroomUsers*') ? 'active' : '' }}">
     </ul>
 </li>
 {{-- <li class="nav-item">
-    <a href="{{ route('questionQuizzes.index') }}"
-class="nav-link {{ Request::is('questionQuizzes*') ? 'active' : '' }}">
-<p>Question Quizzes</p>
-</a>
+    <a href="{{ route('questionQuizzes.index') }}" class="nav-link {{ Request::is('questionQuizzes*') ? 'active' : '' }}">
+        <p>Question Quizzes</p>
+    </a>
 </li>
 <li class="nav-item">
     <a href="{{ route('teachables.index') }}" class="nav-link {{ Request::is('teachables*') ? 'active' : '' }}">
         <p>Teachables</p>
     </a>
-</li> - --}}
-{{-- <li class="nav-item">
-    <a href="{{ route('questionChoiceItems.index') }}"
-class="nav-link {{ Request::is('questionChoiceItems*') ? 'active' : '' }}">
-<p>Question Choice Items</p>
-</a>
-</li> --}}
-{{-- <li class="nav-item">
-    <a href="{{ route('resources.index') }}"
-class="nav-link {{ Request::is('resources*') ? 'active' : '' }}">
-<p>Resources</p>
-</a>
 </li>
-
+-
+<li class="nav-item">
+    <a href="{{ route('questionChoiceItems.index') }}" class="nav-link {{ Request::is('questionChoiceItems*') ? 'active' : '' }}">
+        <p>Question Choice Items</p>
+    </a>
+</li>
+<li class="nav-item">
+    <a href="{{ route('resources.index') }}" class="nav-link {{ Request::is('resources*') ? 'active' : '' }}">
+        <p>Resources</p>
+    </a>
+</li>
 
 <li class="nav-item">
     <a href="{{ route('assignments.index') }}" class="nav-link {{ Request::is('assignments*') ? 'active' : '' }}">
         <p>Assignments</p>
     </a>
-</li> --}}
-{{-- <li class="nav-item">
-    <a href="{{ route('quizAttempts.index') }}"
-        class="nav-link {{ Request::is('quizAttempts*') ? 'active' : '' }}">
+</li>
+<li class="nav-item">
+    <a href="{{ route('quizAttempts.index') }}" class="nav-link {{ Request::is('quizAttempts*') ? 'active' : '' }}">
         <p>Quiz Attempts</p>
     </a>
 </li>
 
-
 <li class="nav-item">
-    <a href="{{ route('teachableUsers.index') }}"
-        class="nav-link {{ Request::is('teachableUsers*') ? 'active' : '' }}">
+    <a href="{{ route('teachableUsers.index') }}" class="nav-link {{ Request::is('teachableUsers*') ? 'active' : '' }}">
         <p>Teachable Users</p>
     </a>
-</li> --}}
-{{-- <li class="nav-item">
-    <a href="{{ route('grades.index') }}"
-       class="nav-link {{ Request::is('grades*') ? 'active' : '' }}">
+</li>
+<li class="nav-item">
+    <a href="{{ route('grades.index') }}" class="nav-link {{ Request::is('grades*') ? 'active' : '' }}">
         <p>Grades</p>
     </a>
 </li>
 
-
 <li class="nav-item">
-    <a href="{{ route('modelHasRoles.index') }}"
-       class="nav-link {{ Request::is('modelHasRoles*') ? 'active' : '' }}">
+    <a href="{{ route('modelHasRoles.index') }}" class="nav-link {{ Request::is('modelHasRoles*') ? 'active' : '' }}">
         <p>Model Has Roles</p>
-    </a>
-</li> --}}
-
-
-{{-- <li class="nav-item">
-    <a href="{{ route('roles.index') }}"
-       class="nav-link {{ Request::is('roles*') ? 'active' : '' }}">
-        <p>Roles</p>
-    </a>
-</li> --}}
-
-
-<li class="nav-item">
-    <a href="{{ route('bookmarks.index') }}"
-       class="nav-link {{ Request::is('bookmarks*') ? 'active' : '' }}">
-        <p>Bookmarks</p>
     </a>
 </li>
 
+<li class="nav-item">
+    <a href="{{ route('roles.index') }}" class="nav-link {{ Request::is('roles*') ? 'active' : '' }}">
+        <p>Roles</p>
+    </a>
+</li>
 
+<li class="nav-item">
+    <a href="{{ route('bookmarks.index') }}" class="nav-link {{ Request::is('bookmarks*') ? 'active' : '' }}">
+        <p>Bookmarks</p>
+    </a>
+</li> --}}
