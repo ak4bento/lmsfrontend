02da12fe730971d17c122b368a2e4e8f41018406
tenriajeng <ilhamtenriajeng03@gmail.com>
2021-05-18 09:45:06 +0800

    flash card coming soon

--- resources/views/frontend/flash_card/index.blade.php
+++ resources/views/frontend/flash_card/index.blade.php
@@ -1,7 +1,7 @@
 @extends('frontend.layouts.app')
 
 @section('content')
-    <div class="container">
+    <div class="container" style="height: 70vh;place-items: center; display: grid;">
          <h1>COMING SOON</h1>
     </div>
 @endsection 
