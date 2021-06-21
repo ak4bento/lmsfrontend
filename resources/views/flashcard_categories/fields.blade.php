4fbaf590610a86c09a4263d8b47fa801311786c2
tenriajeng <ilhamtenriajeng03@gmail.com>
2021-06-21 16:49:14 +0800

    new form input flashcard admin menu 2

--- resources/views/flashcard_categories/fields.blade.php
+++ resources/views/flashcard_categories/fields.blade.php
@@ -20,19 +20,38 @@
 <!-- Parent Id Field -->
 <div class="form-group col-sm-6" id="one" style="display: none;">
     {!! Form::label('parent_id', 'Parent Id:') !!}
-    {!! Form::number('parent_id', null, ['class' => 'form-control']) !!}
+    <select name="parent_id" class="form-control select2" id="parent_id" style="width: 100%;">
+        @foreach (App\Models\FlashcardCategories::where('level',1)->get() as $item)
+        <option value="{{ $item->id }}">
+            {{ $item->category }}
+        </option>
+        @endforeach
+    </select>
+
 </div>
 
 <!-- Second Parent Id Field -->
 <div class="form-group col-sm-6 col-lg-6" id="two" style="display: none;">
     {!! Form::label('second_parent_id', 'Second Parent Id:') !!}
-    {!! Form::number('second_parent_id', null, ['class' => 'form-control']) !!}
+    <select name="second_parent_id" class="form-control select2" id="second_parent_id" style="width: 100%;">
+        @foreach (App\Models\FlashcardCategories::where('level',2)->get() as $item)
+        <option value="{{ $item->id }}">
+            {{ $item->category }}
+        </option>
+        @endforeach
+    </select>
 </div>
 
 <!-- Third Parent Id Field -->
 <div class="form-group col-sm-6 col-lg-6" id="three" style="display: none;">
     {!! Form::label('third_parent_id', 'Third Parent Id:') !!}
-    {!! Form::number('third_parent_id', null, ['class' => 'form-control']) !!}
+    <select name="third_parent_id" class="form-control select2" id="third_parent_id" style="width: 100%;">
+        @foreach (App\Models\FlashcardCategories::where('level',3)->get() as $item)
+        <option value="{{ $item->id }}">
+            {{ $item->category }}
+        </option>
+        @endforeach
+    </select>
 </div>
 
 <!-- Category Field -->
